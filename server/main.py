a5cf52561ccda41955db8224c4954f26acdcf86c
Ara-O <oladipoeyiara@gmail.com>
2025-02-24 14:49:49 -0500

    adding cors

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import json
 import time
 
 app = Flask(__name__)
+CORS(app=app)
 
 def formatResults(courses):
     formattedResult = []
