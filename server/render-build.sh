28c064092d325203c3d95f6e7f31476b7400d0bf
Ara-O <oladipoeyiara@gmail.com>
2025-02-24 21:43:03 -0500

    updating render.sh

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-# Install dependencies (if required)
-apt-get update && apt-get install -y wget unzip curl
+STORAGE_DIR=/opt/render/project/.render
 
-# Find Chromium binary (Render might already have it)
-CHROMIUM_PATH=$(which chromium || which chromium-browser || echo "")
-
-if [ -z "$CHROMIUM_PATH" ]; then
-    echo "Chromium is not available! Install it or use another method."
-    exit 1
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
+else
+  echo "...Using Chrome from cache"
 fi
 
-echo "Chromium found at: $CHROMIUM_PATH"
-
-# Get Chromium version
-CHROME_VERSION=$($CHROMIUM_PATH --version | awk '{print $2}' | cut -d'.' -f1)
-echo "Detected Chromium version: $CHROME_VERSION"
-
-# Download the correct Chromedriver version
-CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
-echo "Downloading Chromedriver version: $CHROMEDRIVER_VERSION"
-wget -q "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip -d $HOME/chromedriver
-chmod +x $HOME/chromedriver/chromedriver
-
-# Export paths
-export CHROME_BIN="$CHROMIUM_PATH"
-export PATH="$HOME/chromedriver:$PATH"
-
-echo "Chromium and Chromedriver setup complete!"
+# be sure to add Chromes location to the PATH as part of your Start Command
+export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
