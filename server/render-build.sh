cc393fcec94024bdeeeeaed82b2042bcc9abe2ec
Ara-O <oladipoeyiara@gmail.com>
2025-03-19 19:49:01 -0400

    reverting back to originalg

--- server/render-build.sh
+++ server/render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
 
-# --- Download and install Google Chrome ---
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
@@ -11,22 +11,10 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src
+  cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
 
-# --- Download and install ChromeDriver ---
-if [[ ! -f $STORAGE_DIR/chrome/chromedriver ]]; then
-  echo "...Downloading ChromeDriver"
-  CHROMEDRIVER_VERSION=116.0.5845.96  # Replace with your actual version
-  wget -P $STORAGE_DIR/chrome https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
-  unzip $STORAGE_DIR/chrome/chromedriver_linux64.zip -d $STORAGE_DIR/chrome
-  chmod +x $STORAGE_DIR/chrome/chromedriver
-  rm $STORAGE_DIR/chrome/chromedriver_linux64.zip
-else
-  echo "...Using ChromeDriver from cache"
-fi
-
-# --- Update PATH ---
-export PATH="${PATH}:${STORAGE_DIR}/chrome/opt/google/chrome:${STORAGE_DIR}/chrome"
+# be sure to add Chromes location to the PATH as part of your Start Command
+# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
